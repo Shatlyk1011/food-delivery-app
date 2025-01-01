feca848ba794400d0085846759056e48220022c4
Abdullayev Shatlyk <gj_wp@mail.ru>
2025-01-01 19:58:13 +0500

    add cities query

---
+++ src/app/services/useCities.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+import axios from "../shared/lib/axios";
+
+import { CITIES_QUERY } from "./query/citiesQuery";
+
+export const useGetCities = () => {
+  const { data: cities } = useQuery<City[]>({
+    queryFn: async () => {
+      const { data } = await axios({
+        data: {
+          query: CITIES_QUERY,
+          variables: { limit: 30 },
+        },
+      });
+      console.log("data.data", data.data);
+      return await data.data.Cities.docs;
+    },
+    staleTime: Infinity,
+    queryKey: ["cities"],
+  });
+
+  return { cities };
+};
