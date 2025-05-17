4cb4657d5bd91085aa48c2361213ff65231b86ce
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-05-18 00:01:14 +0500

    add antialised styles

--- src/app/(pages)/[locale]/layout.tsx
+++ src/app/(pages)/[locale]/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({ children, params }: Props) {
 
   return (
     <html lang={locale || "en"}>
-      <body className={inter.className}>
+      <body className={`${inter.className} antialiased`}>
         <JotaiProvider>
           <NextIntlClientProvider messages={messages}>
             <TanstackQueryProvider>
